84d0eaa147afa9794fb9f6dc3afff548cc8b58c1
chengyanlai <chengyanlai@gmail.com>
2014-05-29 21:24:15 -0700

    Update site

--- code/bash/Git_Config.sh
+++ code/bash/Git_Config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-git config --global user.name "chengyanlai"
+git config --global user.name "chenyenlai"
 git config --global user.email "chengyanlai@gmail.com"
 git config --global color.ui true
 git config --global color.log auto
@@ -7,7 +7,11 @@ git config --global core.editor "mate -w"
 git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
+git config --global alias.bh branch
 git config --global alias.last 'log -1 HEAD'
-git config --global commit.template $HOME/Dropbox/File/git_files/gitmessage.txt
-git config --global core.excludesfile $HOME/Dropbox/File/git_files/gitignore_global
+git config --global alias.slog 'log --oneline -20'
+git config --global alias.clni 'clean -i'
+git config --global commit.template path/to/the/file/gitmessage.txt
+git config --global core.excludesfile path/to/the/file/gitignore_global
 git config --global push.default simple
+git config --global merge.tool opendiff
